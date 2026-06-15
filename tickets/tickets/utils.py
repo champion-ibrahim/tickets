6f946b560391b59514c5d779393457a51ec0b9aa
champion-ibrahim <i.alwajih@championsys.cloud>
2026-06-15 07:36:28 +0000

    lecture 4

---
+++ tickets/tickets/utils.py
@@ -0,0 +1,18 @@
+import frappe
+from frappe.model.workflow import apply_workflow
+
+def new_employee_ticket(doc, method=None):
+    ticket = frappe.get_doc(
+        {
+            "doctype": "Ticket",
+            "subject": f"Set up email and software environment for employee {doc.first_name}",
+            "issue_type": "Software",
+            "description": "Set up email, basic apps, and software environment",
+        }
+    )
+    ticket.flags.ignore_validate = True
+    ticket.insert(ignore_permissions=True)
+    apply_workflow(ticket, "Open")
+
+    
+    
